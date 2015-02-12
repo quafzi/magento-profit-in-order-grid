96e22bc6a3e6b3c271032d6051dab7f652d00c88
Thomas Birke <quafzi@netextreme.de>
2015-02-12 13:52:39 +0100

    Show profit in order view

--- src/app/code/community/Quafzi/ProfitInOrderGrid/Helper/Data.php
+++ src/app/code/community/Quafzi/ProfitInOrderGrid/Helper/Data.php
@@ -9,6 +9,8 @@
 class Quafzi_ProfitInOrderGrid_Helper_Data
     extends Mage_Core_Helper_Data
 {
+    protected $_items = array();
+
     /**
      * get profit of an order
      * 
@@ -18,10 +20,12 @@ class Quafzi_ProfitInOrderGrid_Helper_Data
     public function getProfit(Varien_Object $order)
     {
         $orderProfit = new Varien_Object();
-        $orderProfit->setContainsNegativeProfit(false);
-        $orderProfit->setContainsWrongData(false);
-        $orderProfit->setCost(0);
-        $orderProfit->setProfit(0);
+        $orderProfit
+            ->setContainsNegativeProfit(false)
+            ->setContainsWrongData(false)
+            ->setCost(0)
+            ->setProfit(0);
+        $this->_items[$order->getId()] = array();
         $rowNetPrices = array();
         foreach ($order->getAllItems() as $item) {
             $product = $item->getProduct();
@@ -30,8 +34,13 @@ class Quafzi_ProfitInOrderGrid_Helper_Data
             $rowNetPrice = $item->getRowTotal() - $item->getDiscountAmount();
             if (empty($rowPrice) && $item->getParentItemId()) {
                 $rowNetPrice = $rowNetPrices[$item->getParentItemId()];
+                $this->_items[$order->getId()][$item->getId()] = $this->_items[$order->getId()][$item->getParentItemId()];
             } else {
                 $rowNetPrices[$item->getId()] = $rowNetPrice;
+                $this->_items[$order->getId()][$item->getId()] = new Varien_Object(array(
+                    'cost'      => $rowCost,
+                    'net_price' => $rowNetPrice,
+                ));
             }
             if ($rowNetPrice <= 0) {
                 continue;
@@ -50,10 +59,47 @@ class Quafzi_ProfitInOrderGrid_Helper_Data
                 $orderProfit->setContainsNegativeProfit(true);
             }
             $rowProfit = $rowNetPrice - $rowCost;
-            $orderProfit->setCost($orderProfit->getCost() + $rowCost);
-            $orderProfit->setProfit($orderProfit->getProfit() + $rowProfit);
-            $orderProfit->setIncome($orderProfit->getIncome() + $rowNetPrice);
+            $orderProfit
+                ->setCost($orderProfit->getCost() + $rowCost)
+                ->setProfit($orderProfit->getProfit() + $rowProfit)
+                ->setIncome($orderProfit->getIncome() + $rowNetPrice);
+            if (!isset($this->_items[$order->getId()][$item->getId()])) {
+                $this->_items[$order->getId()][$item->getId()] = new Varien_Object();
+            }
+            $this->_items[$order->getId()][$item->getId()]
+                ->setCost($rowCost)
+                ->setNetPrice($rowNetPrice)
+                ->setProfit($rowProfit);
         }
         return $orderProfit;
+    }
+
+    public function getItemProfit(Mage_Sales_Model_Order_Item $item)
+    {
+        if (empty($this->_items)) {
+            $this->getProfit($item->getOrder());
+        }
+        if (!isset($this->_items[$item->getOrderId()][$item->getId()])) {
+            return '';
+        }
+        $row = $this->_items[$item->getOrderId()][$item->getId()];
+
+        $subTotals = array(
+            $this->__('Net Cost')  => $row->getCost(),
+            $this->__('Net Price') => $row->getNetPrice(),
+        );
+        $out = '<table class="qty-table" cellspacing="0">';
+        foreach ($subTotals as $label=>$value) {
+            $out .= '<tr><td>' . $label . '</td>'
+                . '<td>' . Mage::helper('core')->formatPrice($value) . '</td></tr>';
+        }
+        $profit = $row->getProfit();
+        $profitStyle = ((0 < $profit) ? '' : 'color:red;') . 'font-weight:bold';
+        $out .= '<tr><td>' . $this->__('Profit') . '</td>'
+            . '<td style="' . $profitStyle . '">'
+            . Mage::helper('core')->formatPrice($profit)
+            . '</td></tr>';
+        $out .= '</table>';
+        return $out;
     }
 }
--- src/app/code/community/Quafzi/ProfitInOrderGrid/Model/Observer.php
+++ src/app/code/community/Quafzi/ProfitInOrderGrid/Model/Observer.php
@@ -16,6 +16,43 @@ class Quafzi_ProfitInOrderGrid_Model_Observer
         }
     }
 
+    public function afterBlockToHtml(Varien_Event_Observer $observer) {
+        $block = $observer->getEvent()->getBlock();
+        if ($block instanceof Mage_Adminhtml_Block_Sales_Order_View_Items_Renderer_Default) {
+            $item = $block->getItem();
+            $this->_insertItemGridProfitColumn($observer->getEvent()->getTransport(), $item);
+        } elseif ($block instanceof Mage_Adminhtml_Block_Sales_Order_View_Items) {
+            $item = $block->getItem();
+            $this->_insertItemGridProfitColumnHeader($observer->getEvent()->getTransport());
+        }
+    }
+
+    protected function _insertItemGridProfitColumnHeader($transport)
+    {
+        $html = $transport->getHtml();
+        $html = str_replace('<thead>', '<col width="1" /><thead>', $html);
+        $title = Mage::helper('quafzi_profitinordergrid')->__('Profit');
+        $transport->setHtml(
+            str_replace(
+                '<th class="last">',
+                '<th class="a-center">' . $title . '</th><th class="last">',
+                $html
+            )
+        );
+    }
+    protected function _insertItemGridProfitColumn($transport, $item)
+    {
+        $html = $transport->getHtml();
+        $profit = Mage::helper('quafzi_profitinordergrid')->getItemProfit($item);
+        $transport->setHtml(
+            str_replace(
+                '<td class="a-right last">',
+                '<td class="a-right">' . $profit . '</td><td class="a-right last">',
+                $html
+            )
+        );
+    }
+
     protected function _modifyGrid(Mage_Adminhtml_Block_Widget_Grid $grid, $after='grand_total')
     {
         $this->_addProfitColumn($grid, $after);
